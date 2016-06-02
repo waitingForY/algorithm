330b079077182765428f7ebe2b2ea8f9394eb588
waitingForY <1154043153@qq.com>
2016-06-02 22:12:05 +0800

    explain

--- interview/overloadoperator/string.h
+++ interview/overloadoperator/string.h
@@ -7,8 +7,8 @@ class String
 		explicit String(const char* str);
 		~String();
 		String(const String& other);
-		String& operator=(const String& other);
-		String& operator=(const char* str);
+		String& operator=(const String& other);//赋值运算符的重载
+		String& operator=(const char* str);//等号运算符的重载
 		void display() const;
 	private:
 		char* str_;
