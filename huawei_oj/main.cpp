0d2238db7817ce2077e29bb72b3e6424c6534960
waitingForY <1154043153@qq.com>
2016-07-28 20:33:44 +0800

    debug oj

--- huawei_oj/main.cpp
+++ huawei_oj/main.cpp
@@ -8,7 +8,6 @@ typedef struct treeNode
 	int val;
 	struct treeNode* next;
 }Node;
-
 Node *rootList[MAX_VERTEX];
 vector<int>leafs;
 vector<int>result;
@@ -22,7 +21,6 @@ void initRootList(Node *rootList[],int n)
 		rootList[i]=node;
 	}
 }
-
 void tableToList(int table[][MAX_VERTEX],Node *rootList[],int n,int currentnodeval)
 {
 	if(currentnodeval==1)
@@ -52,44 +50,40 @@ void tableToList(int table[][MAX_VERTEX],Node *rootList[],int n,int currentnodev
 		}
 	}
 }
-
-
 void findPath(int root)
 {
+	if(leafs.size()==0)
+	  return;
 	result.push_back((*(rootList+root-1))->val);
-	if((*rootList+root-1)->val==*leafs.begin())
+	if((*(rootList+root-1))->val==*leafs.begin())
 	  leafs.erase(leafs.begin());
-	Node* p=(*rootList+root-1)->next;
+	Node* p=(*(rootList+root-1))->next;
 	while(p)
 	{
 		findPath(p->val);
-		result.push_back((*rootList+root-1)->val);
-		if((*rootList+root-1)->val==*leafs.begin())
+		result.push_back((*(rootList+root-1))->val);
+		if((*(rootList+root-1))->val==*leafs.begin())
 		  leafs.erase(leafs.begin());
 		p=p->next;
 	}
 }
-
-
-
-void makeTree()
+int main(void)
 {
-	int n=5;
-	//while(cin>>n)
-	//{
+	int n;
+	while(cin>>n)
+	{
 		int tmp[MAX_VERTEX][MAX_VERTEX];
 		for(int i=0;i<n;i++)
 		  for(int j=0;j<n;j++)
 			tmp[i][j]=0;
-		/*for(int i=0;i<n-1;i++)
+		for(int i=0;i<n-1;i++)
 		{
 			int ver1,ver2;
 			cin>>ver1>>ver2;
 			ver1<ver2?tmp[ver1-1][ver2-1]=1:tmp[ver2-1][ver1-1]=1;
-		}*/
-		tmp[0][1]=tmp[0][2]=tmp[1][3]=tmp[1][4]=1;
+		}
 		initRootList(rootList,n);
-		/*int leafcount=0;
+		int leafcount=0;
 		for(int i=1;i<n;i++)
 		{
 			int j;
@@ -101,57 +95,24 @@ void makeTree()
 				leafcount=n-i;
 				break;
 			}
-		}*/
-		/*
+		}
 		int leaf;
 		for(int i=0;i<leafcount;i++)
 		{
 			cin>>leaf;
 			leafs.push_back(leaf);
 			tableToList(tmp,rootList,n,leaf);
-		}*/
-		leafs.push_back(3);
-		leafs.push_back(4);
-		leafs.push_back(5);
-		tableToList(tmp,rootList,n,3);
-		tableToList(tmp,rootList,n,4);
-		tableToList(tmp,rootList,n,5);
-		
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			  cout<<tmp[i][j]<<" ";
-			cout<<endl;
-		}
-
-		for(int i=0;i<n;i++)
-		{
-			cout<<rootList[i]->val<<"->";
-			Node* p=rootList[i]->next;
-			while(p)
-			{	
-				cout<<p->val<<"->";
-				p=p->next;
-			}
-			cout<<"NULL"<<endl;
-
 		}
 		findPath(1);
-	//}
-}
-
-
-int main(void)
-{
-	makeTree();
-	//findPath(rootList);
-	if(leafs.size()==0)
-	  cout<<-1<<endl;
-	else
-	{
-		for(vector<int>::iterator it=result.begin();it!=result.end();++it)
-		  cout<<*it<<"->";
-		cout<<endl;
+		if(leafs.size()!=0)
+		  cout<<-1<<endl;
+		else
+		{
+			for(vector<int>::iterator it=result.begin();it!=result.end();++it)
+			  cout<<*it<<"->";
+			cout<<endl;
+		}
+		result.clear();
 	}
 	return 0;
 }
